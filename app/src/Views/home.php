ae0d811b9e2feb10ec1f9b8c09fb126a5aaf0dca
Aloys Stekkinger <153507008+Sanikku5@users.noreply.github.com>
2025-11-28 15:38:27 +0100

    Add <a> to home page

--- app/src/Views/home.php
+++ app/src/Views/home.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <p>Welcome to the home page of this boiler plate!</p>
-    <a href="../../hello.php">Go to hello.php</a>
-    <a href="../../contact.php">Go to contact.php</a>
+    <p>
+        <a href="/hello.php">Go to hello.php</a>
+    </p>
+    <p>
+        <a href="/contact.php">Go to contact.php</a>
+    </p>
+    <p>
+        <a href="/guestbook">Go to guestbook.php</a>
+    </p>
 </body>
+
 </html>
